0e3d489b59aa99607a34b577f44800c3ca1ea425
Motion Li <287300953@qq.com>
2014-09-07 12:22:10 +0800

    修改配置文件

--- apache/django.wsgi
+++ apache/django.wsgi
@@ -3,7 +3,7 @@ import sys
 path = '/var/www/blog'
 if path not in sys.path:
 	sys.path.insert(0, path)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'conf.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'conf.product'
 
 import django
 django.setup()
--- conf/product.py
+++ conf/product.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-DEBUG = TEMPLATE_DEBUG = True
+DEBUG = TEMPLATE_DEBUG = False
 
 ADMINS = (
     ('Li Maosheng', 'limaoshengcpp@163.com'),
